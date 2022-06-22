fc2bb2116784bfa83446605b73711314452f245c
gamedevturdus <kubaluk757@o2.pl>
2022-06-22 20:52:04 +0200

    initial commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+	name: string;
+	method?: string;
+	body: {
+		message: string;
+	};
+};
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+	res.status(200).json({
+		name: "John Doe",
+		method: req.method,
+		body: req.body,
+	});
 }
